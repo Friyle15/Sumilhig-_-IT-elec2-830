d754a23beddaf1e32b9f71bbf1ca3c63c2aa23e2
git <sumsfrietz@gmail.com>
2023-02-11 13:22:38 +0800

    se

--- app/src/main/java/com/example/helloandriod/MainActivity.java
+++ app/src/main/java/com/example/helloandriod/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.helloandriod;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        Log.v(TAG,"hello world");
+        Log.d(TAG,"hello world");
+        Log.i(TAG,"hello world");
+        Log.w(TAG,"hello world");
+        Log.e(TAG,"hello world");
+
+        Button button = (Button) findViewById(R.id.button2);
+        return button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, " Button Clicked");
+            }
+
+
+        };
+    }}}

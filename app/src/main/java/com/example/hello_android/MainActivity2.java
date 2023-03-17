d95a961d7c9f6037722538811a44930b32377d11
Frietz kyle <sumsfrietz@gmail.com>
2023-03-17 16:42:47 +0800

    aweafc

--- app/src/main/java/com/example/hello_android/MainActivity2.java
+++ app/src/main/java/com/example/hello_android/MainActivity2.java
@@ -16,6 +16,7 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
         firstfragment = (Button) findViewById(R.id.firstfragment);
         secondfragment = (Button) findViewById(R.id.secondfragment);
         firstfragment.setOnClickListener(new View.OnClickListener() {

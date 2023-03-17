b72ca20d04b1dcea9bba1f47447339f4b346baab
Frietz kyle <sumsfrietz@gmail.com>
2023-03-17 17:03:36 +0800

    update

---
+++ app/src/main/java/com/example/helloandroid/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.helloandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button3);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity1.class);
+                startActivity(intent);
+            }
+        });
+    }
+}

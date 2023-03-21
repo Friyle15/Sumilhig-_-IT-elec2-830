c34042ded3f00160ef52ff859e4a4fd16d525b41
Frietz kyle <sumsfrietz@gmail.com>
2023-03-21 23:19:20 +0800

    update

--- app/src/main/java/com/example/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/helloandroid/MainActivity.java
@@ -2,37 +2,43 @@ package com.example.helloandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.nfc.Tag;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final String TAG = "MainActivity";
+    Button firstFragment, secondFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // get the reference of button's
+        firstFragment = (Button) findViewById(R.id.button);
+        secondFragment = (Button) findViewById(R.id.button2);
 
-        Log.v(TAG,"This is a verbose log.");
-        Log.d(TAG,"This is a debug log.");
-        Log.i(TAG,"This is an info log.");
-        Log.w(TAG, "This is a warn log.");
-        Log.e(TAG,"This is an error log");
-
-        Button button = (Button) findViewById(R.id.button6);
-        button.setOnClickListener(new View.OnClickListener() {
+        //setOnClickListener event
+        firstFragment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               Log.i(TAG,"Button Clicked");
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                startActivity(intent);
+                loadFragment(new FragmentA());
             }
         });
-
-
+        //perform setOnClickListener
+        secondFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new FragmentB());
+            }
+        });
+    }
+    private void loadFragment(Fragment fragment)
+    {
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
     }
 }

0b622ff3cfbce0dc0e2288742c1aba650adfb018
Frietz kyle <sumsfrietz@gmail.com>
2023-03-22 23:58:07 +0800

    update

---
+++ app/src/main/java/com/example/helloandroid/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.helloandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button3);
+        Button button1 = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity1.class);
+                startActivity(intent);
+            }
+
+        });
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivity5.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/helloandroid/MainActivity2.java
@@ -0,0 +1,25 @@
+package com.example.helloandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Button button = (Button) findViewById(R.id.button4);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity2.this, MainActivity3.class);
+                startActivity(intent);
+            }
+        });
+    }
+}

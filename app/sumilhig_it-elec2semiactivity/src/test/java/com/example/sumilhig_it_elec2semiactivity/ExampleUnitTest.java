588bc3b1a18a720680a22cca045e6a07fb374b41
Semi first act <sumsfrietz@gmail.com>
2023-04-15 11:43:55 +0800

    15/04/2023

---
+++ app/sumilhig_it-elec2semiactivity/src/test/java/com/example/sumilhig_it_elec2semiactivity/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.sumilhig_it_elec2semiactivity;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
